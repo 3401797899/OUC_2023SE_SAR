888d2834948b76218ecd80acaa01ab039b9550b0
安小白 <3401797899@qq.com>
2023-11-24 18:06:30 +0800

    feat: 模型接入后端

--- sar/utils.py
+++ sar/utils.py
@@ -1,6 +1,8 @@
 import io
 from functools import wraps
 from inspect import isclass
+from pathlib import Path
+
 import numpy as np
 import cv2
 import scipy.io as sio
@@ -8,6 +10,8 @@ from PIL import Image
 from django.urls import path
 from django.views import View
 from django.conf import settings
+
+
 
 
 def router(route, name=""):
@@ -30,22 +34,30 @@ def router(route, name=""):
 
 
 def img_preprocess(img):
-    return cv2.cvtColor(np.array(Image.open(io.BytesIO(img.read()))), cv2.COLOR_BGR2GRAY)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 def convert_result_to_image(result, origin, output_filename):
     for i in range(result.shape[0]):
         for j in range(result.shape[1]):
             result[i][j] = 0 if result[i][j] == 1 else 255
-
+    result = cv2.convertScaleAbs(result)
     edges = cv2.Canny(result, 100, 300)
 
     color_edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
     for i in range(color_edges.shape[0]):
         for j in range(color_edges.shape[1]):
             if np.all(color_edges[i, j] == [0, 0, 0]):
-                color_edges[i, j] = origin[i, j]
+                color_edges[i, j] = origin[0][i, j]
             else:
                 color_edges[i, j] = [0, 0, 255]
-    RESULT_DIR = settings.STATIC_ROOT
-    cv2.imwrite(RESULT_DIR / f"{output_filename}.png", color_edges)
+    cv2.imwrite(str(settings.RESULT_ROOT / f"{output_filename}-1.png"), color_edges)
+    edges = cv2.Canny(result, 100, 300)
+    color_edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    for i in range(color_edges.shape[0]):
+        for j in range(color_edges.shape[1]):
+            if np.all(color_edges[i, j] == [0, 0, 0]):
+                color_edges[i, j] = origin[1][i, j]
+            else:
+                color_edges[i, j] = [0, 0, 255]
+    cv2.imwrite(str(settings.RESULT_ROOT / f"{output_filename}-2.png"), color_edges)
--- sar/views.py
+++ sar/views.py
@@ -1,19 +1,61 @@
+import io
+import os.path
+import threading
+import time
 import uuid
 
+import cv2
+import numpy as np
+from PIL import Image
 from django.conf import settings
 from django.http import JsonResponse
-from .utils import router, img_preprocess
+from django.core.cache import cache
+
+from sar.model import get_result
+from sar.utils import router, img_preprocess, convert_result_to_image
+from django.views import View
 
 
-@router('result', name='result')
-def get_result(request):
-    try:
-        img1 = img_preprocess(request.FILES['img1'])
-        img2 = img_preprocess(request.FILES['img2'])
-        # 跑模型
+@router('sar', name='sar')
+class ResultView(View):
 
-        # 返回
+    def run_model(self, im1, im2):
         result_id = uuid.uuid4()
-        return JsonResponse({'status': 'ok', 'result': settings.DOMAIN + settings.MEDIA_URL + f"{result_id}.png"})
-    except Exception as e:
-        return JsonResponse({'status': 'error', 'msg': str(e)})
+
+        def run(im1, im2):
+            lock = cache.get('lock', False)
+            cache.set(result_id, '等待队列中...', timeout=None)
+            while lock:
+                lock = cache.get('lock', False)
+                time.sleep(0.5)
+            cache.set('lock', True, timeout=None)
+            result = get_result(img_preprocess(im1), img_preprocess(im2), result_id)
+            # cv2.imwrite(str(settings.RESULT_ROOT / f"{result_id}-result.png"), result)
+            convert_result_to_image(result, [im1, im2], result_id)
+            cache.set(result_id, 0, timeout=None)
+            cache.set('lock', False, timeout=None)
+
+        thread = threading.Thread(target=run, args=(im1, im2))
+        thread.start()
+        return result_id
+
+    def get(self, request):
+        id = request.GET.get('id')
+        if not id:
+            return JsonResponse({'status': 'error', 'msg': '请输入id'})
+        if cache.get(id):
+            return JsonResponse({'status': 'ok', 'msg': cache.get(id)})
+        elif os.path.exists(settings.RESULT_ROOT / f"{id}-1.png"):
+            return JsonResponse({'status': 'ok', 'msg': [settings.DOMAIN + settings.RESULT_URL + f"{id}-1.png",
+                                                         settings.DOMAIN + settings.RESULT_URL + f"{id}-2.png"]})
+        else:
+            return JsonResponse({'status': 'error', 'msg': 'id不存在'})
+
+    def post(self, request):
+        try:
+            im1 = np.array(Image.open(io.BytesIO(request.FILES['img1'].read())))
+            im2 = np.array(Image.open(io.BytesIO(request.FILES['img2'].read())))
+            result_id = self.run_model(im1, im2)
+            return JsonResponse({'status': 'ok', 'id': result_id})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'msg': str(e)})
